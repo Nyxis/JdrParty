83c2e873d08d1b7d9169f38aa6f5c32f612ea2fd
claire <115070408+Clairevitsing@users.noreply.github.com>
2024-03-31 20:36:46 +0200

    introduce a chance for failure and fumble

--- src/main/resources/CPP/JdrPartyCPP/GameMaster.cpp
+++ src/main/resources/CPP/JdrPartyCPP/GameMaster.cpp
@@ -1,53 +1,64 @@
 #include "GameMaster.h"
 #include "Logger.h"
-#include <cstdlib>
-#include <ctime>
+#include <random> // For std::mt19937
 #include "GameEventResult.h"
+
+// Declare your random number generator globally or as a class member
+std::mt19937 rng(std::random_device{}());
 
 GameMaster::GameMaster(std::vector<Dice> _dice, std::vector<Coin> _coins, std::vector<Deck> _decks)
         : dice(_dice), coins(_coins), decks(_decks) {}
 
 GameEventResult GameMaster::pleaseGiveMeACrit(double criticalRate, double failureRate, double fumbleRate) {
+    // Use the random number generator to get a random index
+    std::uniform_int_distribution<int> distribution(0, dice.size() + coins.size() + decks.size() - 1);
+    int randomIndex = distribution(rng);
 
-    // Seed the random number generator
-    srand(time(nullptr));
-    int randomIndex = rand() % (dice.size() + coins.size() + decks.size());
     if (randomIndex < dice.size()) {
         // Roll the dice
         Logger::log("Using a dice to generate the result.", Logger::LogType::INFO);
         int rollResult = dice[randomIndex].roll(criticalRate, failureRate, fumbleRate);
-        // Convert the roll result to a Result object
         if (rollResult > 0) {
             return GameEventResult::SUCCESS;
         } else if (rollResult < 0) {
             return GameEventResult::FAILURE;
         } else {
-            return GameEventResult::FUMBLE;
+            // Introduce a chance for fumble
+            if (fumbleRate > 0.0 && rng() % 100 < fumbleRate * 100) {
+                return GameEventResult::FUMBLE;
+            }
+            return GameEventResult::FAILURE;
         }
     } else if (randomIndex < dice.size() + coins.size()) {
         // Flip the coin
         Logger::log("Using a coin to generate the result.", Logger::LogType::INFO);
         bool flipResult = coins[randomIndex - dice.size()].flip(criticalRate, failureRate, fumbleRate);
-        // Convert the flip result to a Result object
         if (flipResult) {
             return GameEventResult::SUCCESS;
         } else {
+            if (fumbleRate > 0.0 && rng() % 100 < fumbleRate * 100) {
+                return GameEventResult::FUMBLE;
+            }
             return GameEventResult::FAILURE;
         }
     } else {
         // Draw from the deck
         Logger::log("Using a deck of cards to generate the result.", Logger::LogType::INFO);
         int drawResult = decks[randomIndex - dice.size() - coins.size()].draw(criticalRate, failureRate, fumbleRate);
-        // Convert the draw result to a Result object
         if (drawResult > 0) {
             return GameEventResult::SUCCESS;
         } else if (drawResult < 0) {
             return GameEventResult::FAILURE;
         } else {
-            return GameEventResult::FUMBLE;
+            // Introduce a chance for fumble
+            if (fumbleRate > 0.0 && rng() % 100 < fumbleRate * 100) {
+                return GameEventResult::FUMBLE;
+            }
+            return GameEventResult::FAILURE;
         }
     }
 }
+
 
 
 
